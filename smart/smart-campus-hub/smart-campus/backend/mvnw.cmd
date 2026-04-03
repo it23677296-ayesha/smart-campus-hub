de60b2c8ad45a68061f3b0cd00b4be9624bcdee7
Ayesha Gayanthi <it23677296@my.sliit.lk>
2026-04-03 09:34:53 +0530

    create facilities form

--- smart/smart-campus-hub/smart-campus/backend/mvnw.cmd
+++ smart/smart-campus-hub/smart-campus/backend/mvnw.cmd
@@ -1,3 +1,3 @@
 @echo off
-set MVN_BIN=C:\Users\ASUS\.m2\wrapper\dists\apache-maven-3.9.11\03d7e36a140982eea48e22c1dcac01d8862b2550b2939e09a0809bbc5182a5bc\bin
+set MVN_BIN=C:\Users\ayesh\.m2\wrapper\dists\apache-maven-3.9.11\03d7e36a140982eea48e22c1dcac01d8862b2550b2939e09a0809bbc5182a5bc\bin
 "%MVN_BIN%\mvn.cmd" %*
